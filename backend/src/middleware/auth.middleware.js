34323497e4627ad9e4fd87e08b5b3ecfe656150d
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-08-09 18:09:26 -0400

    remove server error

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,6 +1,12 @@
-export const protectRoute = async (req , res , next) => {
-    if (!req.auth().isAuthenticated) {
-        return res.status(401).json({"message" : "Unauthorized - you must be logged in"})
-    }
-    next()
+import { getAuth } from '@clerk/express'
+
+export const protectRoute = (req, res, next) => {
+  const { userId, sessionId, getToken } = getAuth(req)
+
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized - you must be logged in" })
+  }
+
+  // User is authenticated, continue to next middleware/handler
+  next()
 }

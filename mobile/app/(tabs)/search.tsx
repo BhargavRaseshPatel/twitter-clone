468e3737683e850ed4a5e45bda04f8a036fdab58
Bhargav Patel <bhargavrpatelcanada@gmail.com>
2025-08-09 13:01:26 -0400

    tab setup completed and signout button

---
+++ mobile/app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearhScreen = () => {
+  return (
+    <View>
+      <Text>SearhScreen</Text>
+    </View>
+  )
+}
+
+export default SearhScreen
